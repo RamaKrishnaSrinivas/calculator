5daeb2c466067b1dee948cea878a63bf9e41b4b2
Rama Krishna Srinivas <orugantirks@gmail.com>
2025-07-03 20:56:19 +0530

    calculator.js

---
+++ calculator.js
@@ -0,0 +1,35 @@
+function btn(value) {
+    var display = document.getElementById('display');
+    display.value += value;
+}
+
+function cleard() {
+    var display = document.getElementById('display');
+    display.value = "";
+}
+function cube() {
+    var display = document.getElementById('display');
+    display.value = (+display.value) **3;
+}
+
+function square() {
+    var display = document.getElementById('display');
+    display.value = (+display.value) ** 2;
+}
+
+function sum() {
+    var display = document.getElementById('display');
+    try {
+        display.value = new Function("return " + display.value)();
+    } catch (error) {
+        display.value = "Error";
+    }
+}
+var mywindow;
+function confirmClose() {
+  if (confirm("Are you sure you want to close this page?")) 
+  {
+    window.close();
+    
+  }
+}
